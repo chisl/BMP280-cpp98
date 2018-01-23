11fb3b7a93a7d99d3988afbaee8927366ae76eed
chisl <info@chisl.io>
2018-01-23 05:41:24 +0100

    update

--- BMP280.cpp
+++ BMP280.cpp
@@ -2,12 +2,17 @@
  * name:        BMP280
  * description: The BMP280 is an absolute barometric pressure sensor especially designed for mobile applications.
  * manuf:       Bosch Sensortec
- * version:     0.1
+ * version:     Version 0.1
  * url:         https://ae-bst.resource.bosch.com/media/_tech/media/datasheets/BST-BMP280-DS001-18.pdf
  * date:        2017-09-02
  * author       https://chisl.io/
  * file:        BMP280.cpp
  */
+
+/*                                                                                                       *
+ *                                   THIS FILE IS AUTOMATICALLY CREATED                                  *
+ *                                    D O     N O T     M O D I F Y  !                                   *
+ *                                                                                                       */
 
 #include "BMP280.hpp"
 
